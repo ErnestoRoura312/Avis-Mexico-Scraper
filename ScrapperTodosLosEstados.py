761de4aff286963f6ba4fef9d3694e36271a5f2e
Ernesto Roura <rourae@gmail.com>
2026-05-05 12:33:50 -0600

    Version 1.1 - Scraper Avis Mexico

--- ScrapperTodosLosEstados.py
+++ ScrapperTodosLosEstados.py
@@ -6,41 +6,101 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
+from openpyxl.utils import get_column_letter
 from time import sleep
 import random
-import re
 import pandas as pd
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
+import sys
 #endregion
 
 #region Funciones
 def pausa():
     sleep(random.uniform(1, 2))
+
+def leerConfiguracion(rutaArchivo):
+    config = {}
+    if not os.path.exists(rutaArchivo):
+        sys.exit(f"ERROR - No se encontró el archivo de configuracion")
+    
+    with open(rutaArchivo, "r") as f:
+        for linea in f:
+            if ":" in linea:
+                clave, valor = linea.split(":")
+                config[clave.strip()] = valor.strip()
+    return config
 #endregion
 
 #region Codigo
 serv = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=serv)
-ruta_script = os.path.dirname(os.path.abspath(__file__))
-ruta_archivo_txt = os.path.join(ruta_script, "Recursos/EstadosMexico.txt")
+rutaScript = os.path.dirname(os.path.abspath(__file__))
+rutaArchivoEstados = os.path.join(rutaScript, "Recursos/EstadosMexico.txt")
+rutaArchivoConfiguracion = os.path.join(rutaScript, "Configuracion/config.txt")
 
 driver.maximize_window()
 
 precios = []
 arreglo_estados = []
 
-fecha_entrega = "2026-3-29" 
-selectorFechaEntrega = f'[data-mdb-date="{fecha_entrega}"]'
-fecha_devolucion = "2026-3-30" 
-selectorFechaDevolucion = f'[data-mdb-date="{fecha_devolucion}"]'
+config = leerConfiguracion(rutaArchivoConfiguracion)
+fechaEntregaStr = config.get("fechaEntrega")
+fechaDevolucionStr = config.get("fechaDevolucion")
 
-with open(ruta_archivo_txt, 'r', encoding='utf-8') as archivo:
-    arreglo_estados = [linea.strip() for linea in archivo if linea.strip()]
+if not fechaEntregaStr or not fechaDevolucionStr:
+    sys.exit("ERROR - Faltan fechas en el archivo de configuración.")
 
-for estado in arreglo_estados:
+hoy = datetime.now()
+fechaLimite = hoy + timedelta(days=365)
+horaLimite = hoy.replace(hour=23, minute=30, second=0, microsecond=0)
+
+try:
+    fEntrega = datetime.strptime(fechaEntregaStr, "%d/%m/%y")
+    fDevolucion = datetime.strptime(fechaDevolucionStr, "%d/%m/%y")
+    
+    if fEntrega.date() < hoy.date():
+        sys.exit(f"ERROR - Fecha invalida: La fecha de entrega ya paso.")
+
+    if fEntrega > fechaLimite or fDevolucion > fechaLimite:
+        sys.exit("ERROR - Fecha invalida: Las fechas no pueden ser mayores a un año desde hoy.")
+
+    if fDevolucion < fEntrega:
+        sys.exit("ERROR - Fecha invalida: La fecha de devolución no puede ser anterior a la de entrega.")
+
+    if fDevolucion > fEntrega + timedelta(days=30):
+        sys.exit("ERROR - Fecha invalida: La fecha de devolución no puede ser mayor de 30 dias a la fecha de entrega.")
+except ValueError:
+    sys.exit("ERROR - Formato de fecha invalido: El formato debe ser DD/MM/YY (ej: 30/04/26).")
+
+try:
+    fEntrega = datetime.strptime(fechaEntregaStr, "%d/%m/%y")
+    fDevolucion = datetime.strptime(fechaDevolucionStr, "%d/%m/%y")
+    
+    if fEntrega.date() == hoy.date():
+        horaEntrega = hoy + timedelta(hours=4)
+        if fDevolucion.date == hoy.date():
+            horaDevolucion = hoy + timedelta(hours=7)
+        else:
+            horaDevolucion = fDevolucion.replace(hour=16, minute=0)
+        
+        if horaEntrega > horaLimite:
+            sys.exit(f"ERROR - No hay tiempo suficiente para generar la renta antes de las 11:30 PM.")
+            
+    else:
+        horaEntrega = fEntrega.replace(hour=12, minute=0)
+        horaDevolucion = fDevolucion.replace(hour=16, minute=0)
+
+    horaEntregaStr = horaEntrega.strftime("%I:%M %p")
+    horaDevolucionStr = horaDevolucion.strftime("%I:%M %p")
+except ValueError:
+    sys.exit("ERROR - No es posible hacer la/las reservas.")
+
+with open(rutaArchivoEstados, 'r', encoding='utf-8') as archivo:
+    arregloEstados = [linea.strip() for linea in archivo if linea.strip()]
+
+for estado in arregloEstados:
     driver.get('https://avis.mx/')
     pausa()
 
@@ -101,16 +161,10 @@ for estado in arreglo_estados:
             }
 
             primeraOpcion.click()
-            fechaEntrega.click()
-            botonDiaEntrega = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, selectorFechaEntrega))
-            )
-            botonDiaEntrega.click()
-            fechaDevolucion.click()
-            botonDiaDevolucion = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, selectorFechaDevolucion))
-            )
-            botonDiaDevolucion.click()
+            fechaEntrega.send_keys(fechaEntregaStr)
+            horaEntrega.send_keys(horaEntregaStr)
+            fechaDevolucion.send_keys(fechaDevolucionStr)
+            horaDevolucion.send_keys(horaDevolucionStr)
             botonBuscar = driver.find_element(By.ID, 'elegirAutoDesk').click()
             WebDriverWait(driver, 15).until(
                 EC.presence_of_element_located((By.CLASS_NAME, 'stepper-head'))
@@ -180,17 +234,67 @@ for estado in arreglo_estados:
 
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-carpeta_principal = "Resultados"
-nombre_subcarpeta = f"precios_{timestamp}"
+carpetaPrincipal = "Resultados"
+nombreSubcarpeta = f"precios_{timestamp}"
 
-ruta_guardado = os.path.join(ruta_script, carpeta_principal, nombre_subcarpeta)
+rutaGuardado = os.path.join(rutaScript, carpetaPrincipal, nombreSubcarpeta)
 
-if not os.path.exists(ruta_guardado):
-    os.makedirs(ruta_guardado)
+if not os.path.exists(rutaGuardado):
+    os.makedirs(rutaGuardado)
 
-ruta_json = os.path.join(ruta_guardado, "precios.json")
+rutaJSON = os.path.join(rutaGuardado, "precios.json")
 
-with open(ruta_json, 'w', encoding='utf-8') as f:
+with open(rutaJSON, 'w', encoding='utf-8') as f:
     json.dump(precios, f, ensure_ascii=False, indent=4)
+
+rows = []
+for entry in precios:
+    estado = entry['Estado']
+    sucursal = entry['Sucursal']
+    
+    if isinstance(entry['Listado_Precios'], str):
+        rows.append({
+            'Estado': estado,
+            'Sucursal': sucursal,
+            'Paquete': 'N/A',
+            'Modelo': 'N/A',
+            'Tarifa Basic': 'N/A',
+            'Tarifa Smart': 'N/A',
+            'Tarifa Plus': 'N/A'
+        })
+        continue
+
+    for item in entry['Listado_Precios']:
+        row = {
+            'Estado': estado,
+            'Sucursal': sucursal,
+            'Paquete': item['Paquete'],
+            'Modelo': item['Modelo']
+        }
+        for tarifa in item['Tarifas']:
+            row.update(tarifa)
+        rows.append(row)
+
+df = pd.DataFrame(rows)
+
+rutaCSV = os.path.join(rutaGuardado, f"precios.csv")
+rutaExcel = os.path.join(rutaGuardado, f"precios.xlsx")
+
+df.to_csv(rutaCSV, index=False, encoding='utf-8-sig')
+
+with pd.ExcelWriter(rutaExcel, engine='openpyxl') as writer:
+    nombreHoja = 'PreciosAvis'
+    df.to_excel(writer, index=False, sheet_name=nombreHoja)
+    
+    worksheet = writer.sheets[nombreHoja]
+    
+    for idx, col in enumerate(df.columns):
+        largoDatos = max(len(str(val)) for val in df[col])
+        largoCabecera = len(str(col))
+        
+        maxLen = max(largoDatos, largoCabecera) + 3
+        colLetter = get_column_letter(idx + 1)
+        
+        worksheet.column_dimensions[colLetter].width = maxLen
 #endregion
 
